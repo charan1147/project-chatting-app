c6e00078f7ef89df79ddd53a5098a16bb687c21a
charan1147 <charanc1147@gmail.com>
2025-07-23 13:12:50 +0530

    first commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const io = setupSocket(server);
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
